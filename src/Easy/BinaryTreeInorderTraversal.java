217fdf33a25633d0d47a7f9debcc83d9b8289ccb
naveenraj88 <rajnaveen0488@gmail.com>
2026-04-24 00:57:53 +0530

    init

---
+++ src/Easy/BinaryTreeInorderTraversal.java
@@ -0,0 +1,26 @@
+package Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeInorderTraversal {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+
+        System.out.println(inorderTraversal(root));
+     }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        result.addAll(inorderTraversal(root.left));
+        result.add(root.val);
+        result.addAll(inorderTraversal(root.right));
+        return result;
+    }
+
+}
---
+++ src/Easy/SymmetricTree.java
@@ -0,0 +1,18 @@
+package Easy;
+
+public interface SymmetricTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        System.out.println(isSymmetric(root));
+    }
+        public static boolean isSymmetric(TreeNode root){
+            return true;
+        }
+}
