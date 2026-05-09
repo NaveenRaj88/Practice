a84e76a3574bf2c49d59303817c4fb8018ff3624
naveenraj88 <rajnaveen0488@gmail.com>
2026-05-09 23:29:30 +0530

    init

--- src/medium/PerfectSquares.java
+++ src/medium/PerfectSquares.java
@@ -2,6 +2,19 @@ package medium;
 
 public class PerfectSquares {
     public static void main(String[] args) {
+        int n = 12;
+        int result = numSquares(n);
+        System.out.println("Minimum number of perfect squares: " + result);
+    }
 
+    public static int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
     }
 }
